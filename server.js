152169b381373c1a898e93e07ed99ce89ae0b684
Vogeln7 <david.vogel.7@gmail.com>
2021-07-07 23:17:11 -0300

    Se crea gitignore, se terminan la rutas get : /items, /item-random, /visitas
    Signed-off-by: Vogeln7 <david.vogel.7@gmail.com>

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
 const express= require('express');
 const fs = require('fs');
 const app=express();
+const encoding="UTF-8";
 
-app.get('/items',(req,res)=>{
-    res.send('hola este es tu servidor express')
+let visitasAItems=0;
+let visitasAItemRandom=0;
+
+app.get('/items', async (req, res)=>{
+    try{
+        const archivo = await fs.promises.readFile('productos.txt',encoding);
+        const productos = JSON.parse(archivo);
+        visitasAItems++;
+        const items = { items: productos, cantidad: productos.length };
+        res.send(items);
+    } catch(err){
+        console.log(err);
+    }
 })
 
-app.get('/item-random',(req,res)=>{
-    res.send('hola este es tu servidor express')
+app.get('/item-random', async (req, res)=>{
+    try{
+        const archivo = await fs.promises.readFile('productos.txt',encoding);
+        const productos = JSON.parse(archivo);
+        visitasAItemRandom++;
+        const numeroRandom = Math.floor(Math.random() * (productos.length));
+        const itemRandom = { item: productos[numeroRandom] };
+        res.send(itemRandom);
+    } catch(err){
+        console.log(err);
+    }
+})
+app.get('/visitas', (req, res)=>{
+    const visitas = {visitas: {items: visitasAItems, itemRandom: visitasAItemRandom}}
+    res.send(visitas);
 })
 
-app.get('/visitas',(req,res)=>{
-    res.send('hola este es tu servidor express')
-})
 
-app.listen(8080,()=>console.log('Se inicia serviio en el puerto 8080'));
+app.listen(8080,()=>console.log('Se inicia servicio en el puerto 8080'));
